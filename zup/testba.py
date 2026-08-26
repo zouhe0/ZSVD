06240be43e025561d7786171926703853dd8da51
zouhe0 <hez157499@gmail.com>
2026-08-26 17:50:55 +0800

    简单修改readme指令

--- zup/testba.py
+++ zup/testba.py
@@ -9,7 +9,7 @@ import sys
 import time
 from mymodel import FusionNet  # 学生模型
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'FusionMamba'))
-from model.u2net import U2Net  # 教师模型
+
 
 def tensor_to_image(tensor):
     """
